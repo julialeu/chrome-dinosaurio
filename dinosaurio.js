80bd0b495a1a56fa3f46d655a963efe8d79cddd7
Julia Leuenberger <leuenberger2101@gmail.com>
2021-12-08 21:03:31 +0100

    I can pause and resume the game

--- dinosaurio.js
+++ dinosaurio.js
@@ -1,4 +1,10 @@
 const player = document.getElementById("player");
+const cactus = document.getElementById("cactus");
+const background = document.getElementById("background");
+
+const buttonPlayStop = document.getElementById("buttonPlayStop");
+let scoreInterval;
+let score = 0;
 
 document.addEventListener("click", function(){
     player.classList.add("playerJump");
@@ -7,3 +13,39 @@ document.addEventListener("click", function(){
 player.addEventListener('animationend',() => {
     player.classList.remove("playerJump");
 })
+
+scoreInterval = setInterval(() => {
+    score++;
+    document.getElementById("score").innerHTML = '<br>' + score
+}, 1000)
+
+function resumeGame() {
+    cactus.style.animationPlayState = 'running';
+    player.style.animationPlayState = 'running';
+    background.style.animationPlayState = 'running';
+
+    scoreInterval = setInterval(() => {
+        score++;
+        document.getElementById("score").innerHTML = '<br>' + score
+    }, 1000)
+}
+
+buttonPlayStop.addEventListener('click', () => {
+    if (buttonPlayStop.classList.contains("play")) {
+        resumeGame();
+    }
+    else {
+        pauseGame();
+    }
+    buttonPlayStop.classList.toggle("play");
+})
+
+function pauseGame() {
+    cactus.style.animationPlayState = 'paused';
+    player.style.animationPlayState = 'paused';
+    background.style.animationPlayState = 'paused';
+    clearInterval(scoreInterval);
+
+
+
+}
