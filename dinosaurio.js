b26543355f60bc0ad50cba192c68690ade7b9ce7
Julia Leuenberger <leuenberger2101@gmail.com>
2021-12-11 21:10:09 +0100

    Fix dino restart

--- dinosaurio.js
+++ dinosaurio.js
@@ -1,135 +1,97 @@
+let score = 0;
+
 const player = document.getElementById("player");
+
+const scoreElement = document.getElementById("score");
 
 const cactus = document.getElementById("cactus");
 
-const background = document.getElementById("background");
+const background = document.getElementById('background');
 
-// const buttonFlow = document.getElementById("button-flow")
+const buttonFlow = document.getElementById("button-flow")
 
-const buttonPlayStop = document.getElementById("buttonPlayStop");
+const board = document.getElementById("board")
+
+let gameLoop;
+
+board.addEventListener("click", function () {
+    player.classList.add("playerJump");
+})
+
+player.addEventListener('animationend', () => {
+    player.classList.remove("playerJump");;
+});
+
+function checkCondition() {
+    if (
+        cactus.offsetLeft < (player.offsetLeft + 49)
+        && cactus.offsetLeft > player.offsetLeft
+        && (player.offsetTop >= (cactus.offsetTop - player.offsetHeight))
+    ) {
+        lostGame();
+    }
+    gameLoop = requestAnimationFrame(checkCondition)
+}
 
 let scoreInterval;
 
-let score = 0;
-
-const board = document.getElementById("board");
-
-// let gameLoop;
-
-
-board.addEventListener('click', function () {
-    playerJump();
-    console.log('foo');
-})
-
-function playerJump() {
-    player.classList.add("playerJump");
-
+function lostGame() {
+    console.log('lostGame');
+    cancelAnimationFrame(gameLoop)
+    pauseGame();
+    buttonFlow.classList.add("reset")
 }
-
-// document.addEventListener("click", function(){
-//     player.classList.add("playerJump");
-// })
-
-player.addEventListener('animationend',() => {
-    removeJump();
-});
-
-function removeJump() {
-    player.classList.remove("playerJump");
-
-}
-
-resumeScore();
-
-function resumeGame() {
-    resumenAnimation();
-    resumeScore();
-}
-
-buttonPlayStop.addEventListener('click', () => {
-    if (buttonPlayStop.classList.contains("play")) {
-        resumeGame();
-    }
-    else {
-        pauseGame();
-    }
-    buttonPlayStop.classList.toggle("play");
-})
 
 function pauseGame() {
-    pauseAnimation();
-    stopScore();
-}
-
-// function lostGame() {
-//     cancelAnimationFrame(gameLoop)
-//     pauseGame();
-//     buttonFlow.classList.add("reset")
-// }
-
-function stopScore() {
-    clearInterval(scoreInterval);
-}
-
-function resumeScore() {
-    scoreInterval = setInterval(() => {
-        score++;
-        document.getElementById("score").innerHTML = '<br>' + score
-    }, 1000)
-}
-
-function pauseAnimation() {
     cactus.style.animationPlayState = 'paused';
     player.style.animationPlayState = 'paused';
     background.style.animationPlayState = 'paused';
+    clearInterval(scoreInterval);
 }
 
-function resumenAnimation() {
+function resumeGame() {
     cactus.style.animationPlayState = 'running';
     player.style.animationPlayState = 'running';
     background.style.animationPlayState = 'running';
 }
 
-const restartButton = document.getElementById("restartGame");
-
-restartButton.addEventListener('click', restartGame)
-
-function restartGame() {
-    resetScore();
-    removeJump();
-    void cactus.offsetWidth;
-    cactus.classList.remove("cactusMovement");
-    cactus.classList.add("cactusMovement");
-
-    // gameLoop = requestAnimationFrame(checkCondition);
-}
-
-// function checkCondition() {
-//     if (
-//         cactus.offsetLeft < (player.offsetLeft + 49)
-//         && cactus.offsetLeft > player.offsetLeft
-//         && (player.offsetTop >= (cactus.offsetTop - player.offsetHeight))
-//     ) {
-//         lostGame();
-//     }
-//     gameLoop = requestAnimationFrame(checkCondition)
-// }
-
 function resetScore() {
     score = 0;
-    document.getElementById("score").innerHTML = '<br>' + score;
+    scoreElement.innerText = score;
+    scoreInterval = setInterval(() => {
+        score++;
+        scoreElement.innerText = score;
+    }, 1000)
 }
 
-document.addEventListener("keyup", (event) => {
-    const key = event.key;
-    if(key == 'ArrowUp') {
-        playerJump();
-    }
+function resetGame() {
+    resetAnimation();
+    resumeGame();
+    resetScore();
+    gameLoop = requestAnimationFrame(checkCondition);
+}
 
-    const keyLowerCase = key.toLowerCase();
-
-    if (keyLowerCase == "w") {
-        playerJump();
+buttonFlow.addEventListener('click', () => {
+    if (buttonFlow.classList.contains('play')) {
+        resumeGame();
+        buttonFlow.classList.remove('play')
     }
+    else if (buttonFlow.classList.contains('reset')) {
+        resetGame();
+        buttonFlow.classList.remove('reset')
+    }
+    else {
+        pauseGame();
+        buttonFlow.classList.add('play')
+    }
+})
+
+function resetAnimation() {
+    cactus.classList.remove("animatedCactus");
+    player.classList.remove("playerJump");
+    void cactus.offsetWidth;
+    cactus.classList.add("animatedCactus");
+}
+window.addEventListener("load", () => {
+    resetGame();
 })
